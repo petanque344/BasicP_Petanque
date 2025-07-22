7e0079640f2baed92f4bd37e2ecfd67c6f5e7eb8
Student <Student@Train11-5-PC140.control.ad.sit.kmutt.ac.th>
2025-07-22 16:08:54 +0700

    Add Class2 workshop

---
+++ class2_workshop.py
@@ -0,0 +1,14 @@
+dd = int(input("Enter your d."))
+
+if dd >=500:
+     print("45B")
+elif dd >=301:
+     print("35 bath")
+elif dd >=101:
+     print("25 bath")
+elif dd >=51:
+     print("15 bath")
+elif dd >=5:
+     print("10 bath")     
+else:
+     print("no")
